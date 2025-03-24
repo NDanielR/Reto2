492bf648a6e17df5f1dff1ca41872db9fbac096a
NDanielR <n.danielramirez.h@outlook.com>
2025-03-24 16:45:29 -0500

    agrego los get-set y intento trabajar con la list de emergencias

--- Reto_2/src/emergencia/Emergencia.java
+++ Reto_2/src/emergencia/Emergencia.java
@@ -1,11 +1,14 @@
 package emergencia;
 
+import java.util.ArrayList;
+
 public class Emergencia {
 
     private TipoEmergencia tipo;
     private String ubicacion;
     private String nivelGravedad;
     private String tiempoRespuesta;
+    private static ArrayList<Emergencia> listaEmergencias = new ArrayList<>();
 
     public Emergencia (TipoEmergencia tipo,String ubicacion, String nivelGravedad, String tiempoRespuesta){
 
@@ -13,6 +16,50 @@ public class Emergencia {
         this.ubicacion = ubicacion;
         this.nivelGravedad = nivelGravedad;
         this.tiempoRespuesta = tiempoRespuesta;
-        
+        listaEmergencias.add(new Emergencia(tipo, ubicacion, nivelGravedad, tiempoRespuesta));   
     }
+    
+    public static void ImprimirListaEmergencias() {
+        if (listaEmergencias.isEmpty()) {
+            System.out.println("No hay emergencias registradas.");
+        } else {
+            for (Emergencia emergencia : listaEmergencias) {
+                System.out.println(emergencia);
+            }
+        }
+    }
+    
+    public TipoEmergencia getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoEmergencia tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getNivelGravedad() {
+        return nivelGravedad;
+    }
+
+    public void setNivelGravedad(String nivelGravedad) {
+        this.nivelGravedad = nivelGravedad;
+    }
+
+    public String getTiempoRespuesta() {
+        return tiempoRespuesta;
+    }
+
+    public void setTiempoRespuesta(String tiempoRespuesta) {
+        this.tiempoRespuesta = tiempoRespuesta;
+    }
+
+
 }

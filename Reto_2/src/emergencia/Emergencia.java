70e1c20a2135f8a354a631d43dd7c4c53e3a6400
NDanielR <n.danielramirez.h@outlook.com>
2025-03-24 21:50:58 -0500

    se agrega interfaz emergencia

--- Reto_2/src/emergencia/Emergencia.java
+++ Reto_2/src/emergencia/Emergencia.java
@@ -1,65 +1,6 @@
 package emergencia;
 
-import java.util.ArrayList;
+public interface Emergencia {
 
-public class Emergencia {
-
-    private TipoEmergencia tipo;
-    private String ubicacion;
-    private String nivelGravedad;
-    private String tiempoRespuesta;
-    private static ArrayList<Emergencia> listaEmergencias = new ArrayList<>();
-
-    public Emergencia (TipoEmergencia tipo,String ubicacion, String nivelGravedad, String tiempoRespuesta){
-
-        this.tipo = tipo;
-        this.ubicacion = ubicacion;
-        this.nivelGravedad = nivelGravedad;
-        this.tiempoRespuesta = tiempoRespuesta;
-        listaEmergencias.add(new Emergencia(tipo, ubicacion, nivelGravedad, tiempoRespuesta));   
-    }
-    
-    public static void ImprimirListaEmergencias() {
-        if (listaEmergencias.isEmpty()) {
-            System.out.println("No hay emergencias registradas.");
-        } else {
-            for (Emergencia emergencia : listaEmergencias) {
-                System.out.println(emergencia);
-            }
-        }
-    }
-    
-    public TipoEmergencia getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoEmergencia tipo) {
-        this.tipo = tipo;
-    }
-
-    public String getUbicacion() {
-        return ubicacion;
-    }
-
-    public void setUbicacion(String ubicacion) {
-        this.ubicacion = ubicacion;
-    }
-
-    public String getNivelGravedad() {
-        return nivelGravedad;
-    }
-
-    public void setNivelGravedad(String nivelGravedad) {
-        this.nivelGravedad = nivelGravedad;
-    }
-
-    public String getTiempoRespuesta() {
-        return tiempoRespuesta;
-    }
-
-    public void setTiempoRespuesta(String tiempoRespuesta) {
-        this.tiempoRespuesta = tiempoRespuesta;
-    }
-
-
+    void atender ();
 }

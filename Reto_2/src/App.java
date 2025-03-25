7630d11762befaf983d8bf2cc60dc6b59454b44b
NDanielR <n.danielramirez.h@outlook.com>
2025-03-24 21:59:02 -0500

    se modifica segun necesidades de pruebas

--- Reto_2/src/App.java
+++ Reto_2/src/App.java
@@ -1,14 +1,12 @@
-import emergencia.Emergencia;
+import emergencia.EmergenciaFactory;
 
 public class App {
     
     public static void main(String[] args) throws Exception {
         
-        Menu.MenuRegistrarEmergencia();
-        Menu.MenuRegistrarEmergencia();
-        Menu.MenuRegistrarEmergencia();
         
-        Emergencia.ImprimirListaEmergencias();
+        
+
         
     }
 }

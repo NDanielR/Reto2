5251042e938bdb08fa97dba3a14c9757eb93dcf7
NDanielR <n.danielramirez.h@outlook.com>
2025-04-23 12:59:39 -0500

    se mueve la solicitud de datos para que el constructor quede libre

--- Reto_2/src/emergencia/EmergenciaFactory.java
+++ Reto_2/src/emergencia/EmergenciaFactory.java
@@ -1,17 +1,41 @@
 package emergencia;
 
+import java.util.Scanner;
+
 import emergencia.inter.InEmergencia;
 import emergencia.inter.imple.AccidenteTransito;
 import emergencia.inter.imple.Incendio;
 import emergencia.inter.imple.Robo;
 import emergencia.inter.imple.TipoEmergencia;
+import util.ScannerSingleton;
 
 public class EmergenciaFactory {
 
     public static InEmergencia crearEmergencia(TipoEmergencia tipo) {
         switch (tipo) {
             case Incendio:
-                return new Incendio();
+            Scanner scn = ScannerSingleton.getInstance();// new Scanner(System.in);
+            System.out.println("Ingrese la ubicacion del incendio: ");
+            String ubicacion = scn.nextLine();
+            System.out.println("Ingrese la descripcion del incendio: ");
+            String descripcion = scn.nextLine();
+            boolean personasAtrapadas = false;
+    
+            while (true) {
+                System.out.println("Hay personas atrapadas en el incendio (si/no): ");
+                String entrada = scn.nextLine().toLowerCase().trim();
+                if (entrada.equals("si")) {
+                    personasAtrapadas = true;
+                    break;
+                } else if (entrada.equals("no")) {
+                    personasAtrapadas = false;
+                    break;
+                } else {
+                    System.out.println("Entrada no válida. Por favor, ingrese 'si' o 'no'.");
+                }
+            }
+            return new Incendio(ubicacion, descripcion, personasAtrapadas);
+
             case Robo:
                 return new Robo();
             case Accidente_Transito:
@@ -19,5 +43,29 @@ public class EmergenciaFactory {
             default:
                 throw new IllegalArgumentException("Tipo de emergencia no válido");
         }
+    }
+
+    public static Incendio crearEmergenciaIncendio() {
+        Scanner scn = ScannerSingleton.getInstance();// new Scanner(System.in);
+        System.out.println("Ingrese la ubicacion del incendio: ");
+        String ubicacion = scn.nextLine();
+        System.out.println("Ingrese la descripcion del incendio: ");
+        String descripcion = scn.nextLine();
+        boolean personasAtrapadas = false;
+
+        while (true) {
+            System.out.println("Hay personas atrapadas en el incendio (si/no): ");
+            String entrada = scn.nextLine().toLowerCase().trim();
+            if (entrada.equals("si")) {
+                personasAtrapadas = true;
+                break;
+            } else if (entrada.equals("no")) {
+                personasAtrapadas = false;
+                break;
+            } else {
+                System.out.println("Entrada no válida. Por favor, ingrese 'si' o 'no'.");
+            }
+        }
+        return new Incendio(ubicacion, descripcion, personasAtrapadas);
     }
 }

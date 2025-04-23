b3ea70dd6ae523aed60eda28bd826de2124d7a29
NDanielR <n.danielramirez.h@outlook.com>
2025-04-23 13:00:49 -0500

    se quita la pedida de datos al usuario desde el constructor

--- Reto_2/src/emergencia/inter/imple/Incendio.java
+++ Reto_2/src/emergencia/inter/imple/Incendio.java
@@ -1,45 +1,24 @@
 package emergencia.inter.imple;
 
-import java.util.Scanner;
 import emergencia.inter.InEmergencia;
-import util.ScannerSingleton;
 
-public class Incendio implements InEmergencia{
+public class Incendio implements InEmergencia {
 
     private String ubicacion;
     private String descripcion;
     private boolean personasAtrapadas;
-    //private String nivelGravedad;
+    // private String nivelGravedad;
 
-    public Incendio() {
-        Scanner scn = ScannerSingleton.getInstance();//new Scanner(System.in);
-        try {
-            System.out.println("Ingrese la ubicacion del incendio: ");
-            this.ubicacion = scn.nextLine();
-            System.out.println("Ingrese la descripcion del incendio: ");
-            this.descripcion = scn.nextLine();
-            
-            while (true) {
-                System.out.println("Hay personas atrapadas en el incendio (si/no): ");
-                String entrada = scn.nextLine().toLowerCase().trim(); 
-                if (entrada.equals("si")) {
-                    this.personasAtrapadas = true;
-                    break;
-                } else if (entrada.equals("no")) {
-                    this.personasAtrapadas = false;
-                    break;
-                } else {
-                    System.out.println("Entrada no válida. Por favor, ingrese 'si' o 'no'.");
-                }
-            }
-        } finally {
-            //scn.close(); // Cierra el Scanner para evitar advertencias
-        }
+    public Incendio(String ubicacion, String descripcion, boolean personasAtrapadas) {
+        this.ubicacion = ubicacion;
+        this.descripcion = descripcion;
+        this.personasAtrapadas = personasAtrapadas;     
+        // this.nivelGravedad = nivelGravedad;
     }
 
     @Override
     public void registrarEmergencia() {
-      
+
     }
 
     @Override

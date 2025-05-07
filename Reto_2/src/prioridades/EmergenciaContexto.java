8822b6c59eca60bd64b02efbfbf0dda65219629b
NDanielR <n.danielramirez.h@outlook.com>
2025-05-06 20:45:45 -0500

    se agregan la administracion de unidades

--- Reto_2/src/prioridades/EmergenciaContexto.java
+++ Reto_2/src/prioridades/EmergenciaContexto.java
@@ -1,15 +1,11 @@
 package prioridades;
 
-import emergencia.*;
-
 public class EmergenciaContexto {
     
     private IEstrategia estrategia;
-    private EmergenciaFactory emergencia;
 	
-	public EmergenciaContexto(IEstrategia estrategia, EmergenciaFactory emergencia) {
+	public EmergenciaContexto(IEstrategia estrategia) {
 		this.estrategia = estrategia;
-        this.emergencia = emergencia;
 	}
 	
 	public void prioridadAlta () {
